8dab5f9dc451b4444b84cb8236c78d18ac0c66b8
SATHEESH KUMAR <43350313+satheeshk787@users.noreply.github.com>
2023-10-10 22:20:04 +0530

    profile page updates.

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,15 +1,47 @@
-import { Box, Button, Modal, Typography } from '@mui/material';
-import React from 'react';
+import { useEffect, useState } from 'react';
+import LayoutRequest from '../../Layout/LayoutRequest';
+
+interface UserInfo {
+  user: {
+    name: string;
+    email: string;
+  };
+}
 
 const Profile: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const layoutRequest = new LayoutRequest();
+
+      try {
+        const response = await layoutRequest.userInfo();
+        if (response?.data.success !== false && response?.data?.user?._id) {
+          setLoading(false);
+          setUserInfo(response?.data);
+        } else {
+          console.error('Error fetching data:', response?.data.error);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <>Loading user info...</>;
+  }
 
   return (
     <div>
-      <div>Name : <b>Nameeee</b></div>
-      <div>Email : <b>email@gmail.com</b></div>
+      <div>Name : <b>{userInfo?.user.name}</b></div>
+      <div>Email : <b>{userInfo?.user.email}</b></div>
     </div>
   );
-  
 };
 
 export default Profile;
